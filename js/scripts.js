296e62bd27f6f11a6fd368501a9c9cfd4af23a75
Caitlyn Mitchell <kikirafiki@gmail.com>
2022-02-09 18:48:18 -0800

    added event to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-  $("form#langSelector").submit(function() {
+  $("form#langSelector").submit(function(event) {
     event.preventDefault();
 
     const sock = $("#sock").val();
@@ -9,31 +9,21 @@ $(document).ready(function(){
     const places = $("#places").val();
     const biggieLine = $("#biggieLine").val();
     const cocktails =$("#cocktails").val();
-
+    $("#javaScript").hide();
+    $("#ruby").hide();
+    $("#python").hide();
     if (sock === "underArmour" && cocktails === "manhattan" ) {
-      $("#javaScript").hide();
-      $("#ruby").hide();
       $("#python").slideToggle();
     } else if (places === "texas" && utensil === "chopsticks" && biggieLine === "line3") {
-      $("#javaScript").hide();
       $("#ruby").fadeToggle();
-      $("#python").hide();
     } else if (places === "florida" && biggieLine === "line1" && cocktails === "champagne" && sock === "nike" && utensil === "spoon") {
       $("#javaScript").slideToggle();
-      $("#ruby").hide();
-      $("#python").hide();
     } else if (places === "texas" && biggieLine === "line3" && cocktails === "moscowMule") {
-      $("#javaScript").hide();
       $("#ruby").fadeToggle();
-      $("#python").hide();
     } else if (biggieLine === "line2" && sock === "underArmour" && places === "nebraska" && utensil === "toothpick" ) {
-      $("#javaScript").hide();
-      $("#ruby").hide();
       $("#python").slideToggle();
     } else { 
       $("#javaScript").slideToggle();
-      $("#ruby").hidee();
-      $("#python").hide();
     };
   });
 });
